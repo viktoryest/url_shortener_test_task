7be4304e8108942d9eda43026f955e23b089f02c
viktoryest <67237779+viktoryest@users.noreply.github.com>
2026-02-08 21:03:43 +0100

    chore(logging): configure application logger

---
+++ app/logger.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+    ]
+)
+
+logger = logging.getLogger(__name__)
